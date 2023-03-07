11ffb48f5767485ad47005194fdded7d6488e7f4
ScoobsSack <92175669+ScoobsSack@users.noreply.github.com>
2023-03-07 12:25:32 +0100

    made the code more complicated, yay

--- main.c
+++ main.c
@@ -4,6 +4,16 @@
 #define LARGEST_VALUE   15.5
 #define BIAS            3
 
+#define FLOAT8_EXPONENT
+
+const float floats[] = {
+     //   0.001f, -20, 55.8f, // 0 000 0000, 1 111 0000, 0 111 1110
+       // 15.5f, 15.49f, 14.9f, // 0 110 1111, 0 110 1110, 0 110 1101
+        0.25f, -0.25f - 0.015625f * 3, 0.25f + 0.015625f * 3.5f,// 0 001 0000, 1 001 0011, 0 001 0011
+        // Denormalized
+        0.234375f, 0.015625f, 0.015625f*12// 0 000 1111, 0 000 0001, 0 000 1100
+};
+
 struct Float8_s{
     char data;
 } Float8_default = {0};
@@ -23,36 +33,25 @@ int     is_zero(float f)   { return (f < SMALLEST_VALUE && f > -SMALLEST_VALUE);
 int main() {
     Float8 f = Float8_default;
 
-    float floats[] = {
-            0.001f, 0, -20, 55.8f, // infs and 0s
-            15.5f, 14.9f, // 0 110 1111, 0 110 1101
-            0.25f, -0.25f - 0.015625f * 3, // 0 001 0000, 1 001 0011
-            // Denormalized
-            0.234375f, 0.015625f, 0.015625f*12 // 0 000 1111, 0 000 0001, 0 000 1100
-    };
     int length = sizeof(floats)/sizeof(floats[0]);
 
     for(int i = 0; i < length; i++) {
         printf("%f\t", floats[i]);
         float_to_float8(&f, floats[i]);
         print_bits(f.data);
+
+        if((i+1) % 3 == 0) printf("\n");
     }
     return 0;
 }
 
 void float_to_float8(Float8 *out, float f) {
-
-    int exponent, mantissa;
-
     out->data = get_sign(&f);
 
     if(outsideLimits(out, f)) return;
 
-    exponent = get_exponent(&f);
-    mantissa = get_mantissa(f, exponent);
-
-    out->data |= (exponent+BIAS) << 4;
-    out->data |= (mantissa) << 0;
+    out->data |= (get_exponent(&f) << 4);
+    out->data |= get_mantissa(f, (int)((out->data & 0x70) >> 4)) << 0;
 }
 
 int get_sign(float* f) {
@@ -72,13 +71,14 @@ int get_exponent(float* f) {
         *f *= 2;
     }
     // Check if normalized or denormalized .
-    return (exponent <= -BIAS) ? -BIAS : exponent;
+    return exponent+BIAS;
+    //return (exponent <= -BIAS) ? 0 : exponent+BIAS;
 }
 
 int get_mantissa(float f, int exp) {
     int mantissa = 0;
 
-    for(int i = (exp == -BIAS) ? 3 : 4; i >= 0; i--) { // if denorm i = 3 else i = 4
+    for(int i = (exp == 0) ? 3 : 4; i >= 0; i--) { // if denorm i = 3 else i = 4
         if(f >= 1) {
             mantissa |= 1 << i;
             f--;
@@ -91,14 +91,17 @@ int get_mantissa(float f, int exp) {
 int outsideLimits(Float8 *out, float f) {
     if(is_inf(f) || is_zero(f)) {
         if(is_inf(f)) out->data |= 0x70; // set inf bits
+        else out->data = 0;
         return 1;
     }
     else return 0;
 }
 
 void print_bits(unsigned int x) {
-    for (int i = 7; i >= 0; i--)
+    for (int i = 7; i >= 0; i--){
         putchar(((x >> i) & 1U) ? '1' : '0');
+        if(i == 7 || i == 4) putchar(' ');
+    }
     putchar('\n');
 }
 // 110 = 15.5    < x < 8 (0.5)
